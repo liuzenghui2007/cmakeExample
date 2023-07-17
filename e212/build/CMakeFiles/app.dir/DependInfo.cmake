1c5d54e3d98ad5db6d64df5915018ef3818e568f
zenghui <240164005@qq.com>
2023-07-17 17:18:19 +0800

    a

---
+++ e212/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zenghui/polyseq/cmakeExample/e212/add.c" "CMakeFiles/app.dir/add.c.o" "gcc" "CMakeFiles/app.dir/add.c.o.d"
+  "/home/zenghui/polyseq/cmakeExample/e212/div.c" "CMakeFiles/app.dir/div.c.o" "gcc" "CMakeFiles/app.dir/div.c.o.d"
+  "/home/zenghui/polyseq/cmakeExample/e212/main.c" "CMakeFiles/app.dir/main.c.o" "gcc" "CMakeFiles/app.dir/main.c.o.d"
+  "/home/zenghui/polyseq/cmakeExample/e212/mult.c" "CMakeFiles/app.dir/mult.c.o" "gcc" "CMakeFiles/app.dir/mult.c.o.d"
+  "/home/zenghui/polyseq/cmakeExample/e212/sub.c" "CMakeFiles/app.dir/sub.c.o" "gcc" "CMakeFiles/app.dir/sub.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
